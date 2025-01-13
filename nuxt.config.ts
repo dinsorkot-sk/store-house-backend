e48c17746e4eba06b12a7cc79660eeb87a2f065d
dinsorkot-sk <sk.dinsorkot@gmail.com>
2025-01-13 12:12:20 +0700

    🌱 set config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,5 +12,18 @@ export default defineNuxtConfig({
       debug: process.env.DEBUG || false,
     },
   },
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css',
+        },
+      ],
+      title: 'font-end',
+    },
+  },
 });
